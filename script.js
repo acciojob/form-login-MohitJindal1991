7151e3e67994d2e7a9f789d4069073f27a07c4d1
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-28 10:15:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	const First=document.querySelector("#fname");
+	const Last=document.querySelector("#lname");
+
+	alert(`${First.value} ${Last.value}`);
 
 }
